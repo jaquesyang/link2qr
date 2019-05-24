0b0008ba4d95278bc3c8d12ff7d1753129d396dd
Jaques Yang <jaquesyang@192.168.1.8>
2019-05-24 16:38:50 +0800

    format code

--- background.js
+++ background.js
@@ -1,55 +1,48 @@
-
-function generateQRCode(text){
+function generateQRCode(text) {
     text = encodeURIComponent(text);
-    
+
     //var prefix='http://qr.topscan.com/api.php?w=250&text=';
-    var prefix='qr.html#';
-    var url=prefix + text;
+    var prefix = 'qr.html#';
+    var url = prefix + text;
     console.log(url);
-    chrome.windows.create({
-        url:url,
-        width:400,
-        height:400
-    })
+    chrome.windows.create({ url: url, width: 400, height: 400 });
 }
 
-function selectionOnClick(info,tab){
+function selectionOnClick(info, tab) {
     console.log(info.selectionText);
     generateQRCode(info.selectionText);
 }
 
-function linkOnClick(info,tab){
+function linkOnClick(info, tab) {
     console.log(info.linkUrl);
     generateQRCode(info.linkUrl);
 }
 
-function imageOnClick(info,tab){
+function imageOnClick(info, tab) {
     console.log(info.srcUrl);
     generateQRCode(info.srcUrl);
 }
 
-
-var contexts=["selection","link","image"];
-for(var i=0;i<contexts.length;i++){
+var contexts = ['selection', 'link', 'image'];
+for (var i = 0; i < contexts.length; i++) {
     var ctx = contexts[i];
-    var title='Generate selection text QR Code';
+    var title = 'Generate selection text QR Code';
     var fnc = selectionOnClick;
-    switch(i){
+    switch (i) {
         case 1:
-            title='Generate link url QR code';
+            title = 'Generate link url QR code';
             fnc = linkOnClick;
             break;
         case 2:
-            title='Generate Image URL QR code';
+            title = 'Generate Image URL QR code';
             fnc = imageOnClick;
             break;
     }
 
     chrome.contextMenus.create({
-        title:title,
-        contexts:[ctx],
-        onclick:fnc
+        title: title,
+        contexts: [ctx],
+        onclick: fnc
     });
     console.log(ctx);
-
 }
--- qr.js
+++ qr.js
@@ -1,40 +1,38 @@
-
 /*
 function containsEncodedComponents(x) {
     // ie ?,=,&,/ etc
     return (decodeURI(x) !== decodeURIComponent(x));
   }
 */
-  function toUtf8(str) {
+function toUtf8(str) {
     var out, i, len, c;
-    out = "";
+    out = '';
     len = str.length;
     for (i = 0; i < len; i++) {
         c = str.charCodeAt(i);
-        if ((c >= 0x0001) && (c <= 0x007F)) {
+        if (c >= 0x0001 && c <= 0x007f) {
             out += str.charAt(i);
-        } else if (c > 0x07FF) {
-            out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
-            out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F));
-            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
+        } else if (c > 0x07ff) {
+            out += String.fromCharCode(0xe0 | ((c >> 12) & 0x0f));
+            out += String.fromCharCode(0x80 | ((c >> 6) & 0x3f));
+            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3f));
         } else {
-            out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F));
-            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
+            out += String.fromCharCode(0xc0 | ((c >> 6) & 0x1f));
+            out += String.fromCharCode(0x80 | ((c >> 0) & 0x3f));
         }
     }
     return out;
 }
 
-  
-jQuery(document).ready(function () {
-    var text =  window.location.hash.substring(1);
-    
+jQuery(document).ready(function() {
+    var text = window.location.hash.substring(1);
+
     console.log(text);
     //if(containsEncodedComponents(text)){
-        text = toUtf8(decodeURIComponent(text));
+    text = toUtf8(decodeURIComponent(text));
     //}
     console.log(text);
     jQuery('#qr').qrcode({
-            text:text
-        });
+        text: text
+    });
 });
